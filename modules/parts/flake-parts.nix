55fad552e344345eeee0fff6a7e734341cdd0e80
Denis Bekasow <denis.bekasow@dmi.de>
2026-03-08 18:09:18 +0100

    feat: add parts api

--- modules/parts/flake-parts.nix
+++ modules/parts/flake-parts.nix
@@ -1,6 +1,14 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  flake.lib.parts = lib.pipe inputs.import-tree [
+    (i: i.addPath ../parts)
+    (i: i.addAPI {
+      without = self: name:
+        self.filterNot (lib.hasSuffix "${name}.nix");
+    })
+  ];
 
   debug = true;
 }
